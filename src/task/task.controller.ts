f75995854b546b4889cea064a98ab26eb1ea0097
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-11-30 16:51:11 -0500

    crud complet with controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,50 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put, UsePipes, ValidationPipe } from "@nestjs/common";
+import { TaskService } from "./task.service";
+import { CreateTaskDTO } from "./dto/create-task.dto";
+import { UpdateTaskDTO } from "./dto/update-task.dto";
 
 @Controller('tasks')
 export class TaskController{
-    
+    constructor(private readonly task: TaskService){}
+
+    @Get()
+    async getAllTasks(){
+        try {
+            const taskFound = this.task.getAllTasks()
+            return taskFound;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    @Get(':id')
+    async getTaskById(@Param('id') id:string){
+        try {
+            const taskFound = this.task.getTaskById(Number(id))
+            return taskFound
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    async createTask(@Body() task:CreateTaskDTO){
+        try {
+            const taskCreate = this.task.createTask(task)
+            return taskCreate
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    @Put(':id')
+    async updateTask(@Param('id') id:string, @Body()task:UpdateTaskDTO){
+        try {
+            const updateTask = this.task.updateTask(Number(id), task)
+            return updateTask
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
